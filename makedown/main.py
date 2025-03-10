7b8ec25a91be57c7d1e0398c550c95101f230be8
Luis Cruz <luismirandacruz@gmail.com>
2025-03-10 20:45:34 +0100

    add support to stdin and update gitignore

--- makedown/main.py
+++ makedown/main.py
@@ -1,13 +1,24 @@
+import sys
+
 import yaml
 import subprocess
 import click
 
 @click.command()
-@click.argument("filename", type=click.Path(exists=True))
+@click.argument("filename", type=click.Path(exists=True), required=False)
 def cli(filename):
     """Reads a Markdown file, extracts the command from the YAML front matter, and executes it."""
-    with open(filename, "r") as f:
-        content = f.readlines()
+    if filename:
+        with open(filename, 'r') as file:
+            content = file.readlines()
+    else:
+        # If no input_file is provided, read from stdin
+        content = sys.stdin.readlines()
+    
+    # Check if content is empty before processing further
+    if not content:
+        click.echo("No content received.")
+        return
 
     # Look for YAML front matter
     if content[0].strip() == "---":
